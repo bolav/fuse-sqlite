51cd7d8f8c4935b3338910ca8fa645ed616c2c53
Bjørn-Olav Strand <bo@startsiden.no>
2016-05-06 10:00:07 +0200

    updating travis files

--- .travis/install-fuse.sh
+++ .travis/install-fuse.sh
@@ -7,7 +7,7 @@
 
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 	# In /Users/travis/build/bolav/fuse-travis
-	ls -l /Applications/Fuse.app/Contents/Uno/uno.exe
+	echo "Installing Fuse version ${FUSE_VERSION}"
 	wget https://api.fusetools.com/fuse-release-management/releases/${FUSE_VERSION}/osx
 	mv osx fuse_osx_${FUSE_VERSION}.pkg
 	sudo installer -pkg fuse_osx_${FUSE_VERSION}.pkg -target /
